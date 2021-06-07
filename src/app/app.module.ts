c5e64c125d84483cdc055ef4fe792c88491c1e68
muthangashem <muthangashem@gmail.com>
2021-06-07 23:02:43 +0300

    style filters component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './shared/cart/cart.component';
+import { FiltersComponent } from './shared/filters/filters.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ItemCardComponent } from './shared/item-card/item-card.component';
@@ -14,7 +16,9 @@ import { WineshopComponent } from './wineshop/wineshop.component';
     WineshopComponent,
     FooterComponent,
     HeaderComponent,
-    ItemCardComponent
+    ItemCardComponent,
+    CartComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
